dc118ed7a7ae88e08e85b792e211bf9b2963f305
Olivier Pineau <opineau@mirego.com>
2018-02-10 13:28:16 -0500

    add framework + fix scheme

---
+++ DGElasticPullToRefresh/DGElasticPullToRefresh.h
@@ -0,0 +1,19 @@
+//
+//  DGElasticPullToRefresh.h
+//  DGElasticPullToRefresh
+//
+//  Created by Olivier Pineau on 2018-02-10.
+//  Copyright © 2018 Danil Gontovnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGElasticPullToRefresh.
+FOUNDATION_EXPORT double DGElasticPullToRefreshVersionNumber;
+
+//! Project version string for DGElasticPullToRefresh.
+FOUNDATION_EXPORT const unsigned char DGElasticPullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGElasticPullToRefresh/PublicHeader.h>
+
+
